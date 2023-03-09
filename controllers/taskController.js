f677743554fb514d00c8e4aadd1d2300007d6da8
Nate Stemo <natestemo@hotmail.com>
2023-03-09 12:52:27 -0600

    status codes and refactoring controller

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -4,34 +4,33 @@ const asyncHandler = require('express-async-handler')
 const getTasks = asyncHandler( async (req,res) =>{
     const tasks = await Task.find({})
 
-    res.send(tasks)
+    res.status(200).send(tasks)
 })
 
 const addTask = asyncHandler( async (req,res) =>{
     const {text, complete} = req.body;
     if(!text){
         res.status(400).send('no task')
-        throw new Error('there needs to a task')
+        throw new Error('there needs to be a task')
     }
     const task = await Task.create({
         text,
         complete
     })
     if(task){
-        res.status(200).send(`created a task... ${task}`)
+        res.status(200).send(task)
     }
 })
 
 const updateTask = asyncHandler( async (req,res) =>{
-    const id = req.params.id;
     const {text, complete} = req.body;
-    const updatedTask = await Task.findByIdAndUpdate({_id: id},{
+    const updatedTask = await Task.findByIdAndUpdate({_id: req.params.id},{
         text,
         complete
     },{new:true}) 
 
     if(updatedTask){
-        res.status(200).send(`updated task... ${updatedTask}`)
+        res.status(200).send(updatedTask)
     }
     
     res.status(400).send(`task not found`)
@@ -39,8 +38,7 @@ const updateTask = asyncHandler( async (req,res) =>{
 })
 
 const deleteTask = asyncHandler( async (req,res) =>{
-    const id = req.params.id;
-    const deletedTask = await Task.findByIdAndDelete({_id: id}) 
+    const deletedTask = await Task.findByIdAndDelete(req.params.id) 
 
     if(deletedTask){
         res.status(200).send(`deleted task ${id}`)
