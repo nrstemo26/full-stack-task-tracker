5bd06fbf598aa886129cb82a48b4cfe76b1e897a
Nate Stemo <natestemo@hotmail.com>
2023-03-09 12:48:20 -0600

    controller can CRUD with db

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -7,15 +7,48 @@ const getTasks = asyncHandler( async (req,res) =>{
     res.send(tasks)
 })
 
-const addTask = (req,res) =>{
-    res.send('add task')
-}
-const updateTask = (req,res) =>{
-    res.send(`update id ${req.params.id}`)
-}
-const deleteTask = (req,res) =>{
-    res.send(`delete id ${req.params.id}`)
-}
+const addTask = asyncHandler( async (req,res) =>{
+    const {text, complete} = req.body;
+    if(!text){
+        res.status(400).send('no task')
+        throw new Error('there needs to a task')
+    }
+    const task = await Task.create({
+        text,
+        complete
+    })
+    if(task){
+        res.status(200).send(`created a task... ${task}`)
+    }
+})
+
+const updateTask = asyncHandler( async (req,res) =>{
+    const id = req.params.id;
+    const {text, complete} = req.body;
+    const updatedTask = await Task.findByIdAndUpdate({_id: id},{
+        text,
+        complete
+    },{new:true}) 
+
+    if(updatedTask){
+        res.status(200).send(`updated task... ${updatedTask}`)
+    }
+    
+    res.status(400).send(`task not found`)
+    throw new Error('no task with that id')
+})
+
+const deleteTask = asyncHandler( async (req,res) =>{
+    const id = req.params.id;
+    const deletedTask = await Task.findByIdAndDelete({_id: id}) 
+
+    if(deletedTask){
+        res.status(200).send(`deleted task ${id}`)
+    }
+    
+    res.status(400).send(`task not found`)
+    throw new Error('no task with that id')
+})
 
 
 module.exports = {
