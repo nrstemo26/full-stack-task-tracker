b9407ff39ad31bdfbc53efc8e0fe899b7f254cf3
Nate Stemo <natestemo@hotmail.com>
2023-04-16 15:01:39 -0500

    checkbox sets the value for complete

--- client/src/components/AddTaskForm.js
+++ client/src/components/AddTaskForm.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
 
 function AddTaskForm(){
+    const [complete,setComplete] = useState(false);
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        // console.log('email',email,'password: ',password)
+        //other logic
+    }
     return(
-        <div>
-            add tasks
-        </div>
+        <form onSubmit={handleSubmit}  className='form form--login'>
+            <label className='form__input'>
+                Text: 
+                <input type='input' value={description} onChange={(e)=> setDescription(e.target.value)} />
+            </label>
+            <label className='form__input'>
+                Done? 
+                <input type='checkbox' value={complete} onChange={(e)=> setComplete(e.target.checked)} />
+            </label>
+
+            <button type='submit' className='form__btn'>Add Task</button>
+        </form>
     )
 }
 
