0e4f27125b468422dcd804c6afb358cbdd414933
Nate Stemo <natestemo@hotmail.com>
2023-04-17 10:28:21 -0500

    form validation and resets when submitted

--- client/src/components/AddTaskForm.js
+++ client/src/components/AddTaskForm.js
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 function AddTaskForm(){
-    const [complete,setComplete] = useState(false);
+    const [complete, setComplete] = useState(false);
     const [description, setDescription] = useState('');
+
+    useEffect(()=>{
+        console.log('in use effect')
+        const checkbox = document.getElementById('add-checkbox')
+        checkbox.checked = complete;
+
+    },[complete]);
 
     const handleSubmit = (e)=>{
         e.preventDefault();
+        
+        
+        if(description){
+            console.log('you created a task')
+            //send request to server at this point
+            console.log(complete, description)
+            setComplete(false)
+            setDescription('')
+            
+        }
         // console.log('email',email,'password: ',password)
         //other logic
+
     }
+
     return(
         <form onSubmit={handleSubmit}  className='form form--login'>
             <label className='form__input'>
@@ -17,7 +37,7 @@ function AddTaskForm(){
             </label>
             <label className='form__input'>
                 Done? 
-                <input type='checkbox' value={complete} onChange={(e)=> setComplete(e.target.checked)} />
+                <input type='checkbox' id='add-checkbox' value={complete} onChange={(e)=> setComplete(e.target.checked)} />
             </label>
 
             <button type='submit' className='form__btn'>Add Task</button>
