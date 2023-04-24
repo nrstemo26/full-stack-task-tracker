8afd996f7bdda151f5ef281a30a6241ef0c0eaa3
Nate Stemo <natestemo@hotmail.com>
2023-04-24 14:32:28 -0500

    added delete functionality with redux

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,35 +1,28 @@
-import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { deleteTask } from '../features/tasks/taskSlice'
 
 function Task ({ item }) {
-
-    const handleClick = () => {
-        const local = JSON.parse(localStorage.getItem('user'))
+    const dispatch = useDispatch()
     
-        console.log('component clicked')
-        console.log(item._id)
-        const taskUrl = 'http://localhost:5000/api/tasks/' + item._id;
-        
-        axios.patch(taskUrl, {
-            item,
-            complete: !item.complete
-        },{
-            headers: {
-                Authorization: `Bearer ${local.token}`
-            }
-        }).then(res=>{
-            console.log(res)
-        }).catch(err=>{
-            console.log(err)
-        })
-      };
+    const handleComplete = ()=>{
+        console.log('finished now')
+    }
+    const handleDelete = () => {
+        dispatch(deleteTask(item._id))
+    }
 
     return(
-        <div className="task-card" onClick={e => handleClick(e)}  >
+        // <div className="task-card" onClick={e => handleClick(e)}  >
+        <div className="task-card"  >
             <h1>{item.text}</h1>
-            <h2>{item.complete? 'done':'not done'}</h2>
+            <h2>{item.complete? 'done':'not done'}
+            <button className='btn' onClick={() => handleComplete()}>finished</button>
+            </h2>
             <h3>ID: {item._id}</h3>
+            <button className='btn btn-delete' onClick={()=> handleDelete()}>X</button>
         </div>
     )
 }
-//handle classes for 
+
+
 export default Task
