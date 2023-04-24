92ff7a5ded08e0a9406f435f8cfc28f057c87768
Nate Stemo <natestemo@hotmail.com>
2023-04-24 14:09:00 -0500

    refactoring useSelector

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,11 +1,9 @@
 import Task from './Task'
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
-import { getTasks, reset } from '../features/tasks/taskSlice'
+import { getTasks } from '../features/tasks/taskSlice'
 
-// import { useSelector, useDispatch } from 'react-redux';
-// import { getTasks } from '../features/tasks/taskSlice';
 
 
 
@@ -13,33 +11,19 @@ function Tasks () {
     const dispatch = useDispatch();
     const { user } = useSelector((state) => state.auth)
 
-    const {tasks, isLoading, isError, message} = useSelector(
+    // const {tasks, isLoading, isError, message} = useSelector(
+    const { tasks } = useSelector(
         (state) => state.task
     )
 
-    const local = JSON.parse(localStorage.getItem('user'))
-    // console.log(local)
-    // const [tasks, setTasks] = useState([])
     
     useEffect(()=>{
         dispatch(getTasks())
-
-        // axios.get('http://localhost:5000/api/tasks/',{
-        //     headers: {
-        //         Authorization: `Bearer ${local.token}`
-        //     }
-        // })
-        //     .then(res=>{
-        //         setTasks(res.data);
-        //     })
-        //     .catch(err=>{
-        //         console.log(err);
-        //     })
     },[user, dispatch])
 
     return(
         <div>
-            <h1>Welcome {local.name}!</h1>
+            <h1>Welcome {user.name}!</h1>
             {tasks.length > 0 ? (tasks.map((item) => {
                 return <Task 
                 key={item._id} 
